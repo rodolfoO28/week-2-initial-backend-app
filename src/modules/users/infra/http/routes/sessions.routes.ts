7e77b6081cfe6339bc38a1472b8a87d6a4cfc49c
Rodolfo Oliveira <rdf2806@gmail.com>
2020-05-12 22:50:53 -0300

    feat: Add validation in routes using celebrate

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import SessionsController from '../controllers/SessionsController';
 
 const appointmentsRouter = Router();
 const sessionsController = new SessionsController();
 
-appointmentsRouter.post('/', sessionsController.create);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionsController.create,
+);
 
 export default appointmentsRouter;
